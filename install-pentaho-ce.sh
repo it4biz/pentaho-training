b2471c840d7049786ad37736293c30a2b7cfbf3e
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-08-13 11:45:36 +0200

    install-pentaho-ce.sh added

---
+++ install-pentaho-ce.sh
@@ -0,0 +1,26 @@
+
+echo "Welcome"
+
+wget http://sourceforge.net/projects/pentaho/files/Business%20Intelligence%20Server/5.4/biserver-ce-5.4.0.1-130.zip/download
+
+unzip download
+
+echo "BI Server Downloaded and Unzip"
+
+echo "PRD 5.4.0.1-130"
+
+wget http://sourceforge.net/projects/pentaho/files/Report%20Designer/5.4/prd-ce-5.4.0.1-130.zip/download
+
+echo "PDI 5.4.0.1-130 Stable"
+
+wget http://sourceforge.net/projects/pentaho/files/Data%20Integration/5.4/pdi-ce-5.4.0.1-130.zip/download
+
+echo "PSW 3.10.0.1-130 Stable"
+
+wget http://sourceforge.net/projects/mondrian/files/schema%20workbench/3.10.0/psw-ce-3.10.0.1-130.zip/download
+
+echo "PAD 5.4.0.1-130 Stable"
+
+wget http://sourceforge.net/projects/mondrian/files/aggregation%20designer/5.4/pad-ce-5.4.0.1-130.zip/download
+
+
